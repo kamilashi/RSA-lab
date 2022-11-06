dc929003e16a22f1d7540284c3e38cbad09a2f49
Kamila <64157493+kamilashi@users.noreply.github.com>
2022-11-06 17:12:48 +0100

    squareAndMult - binary in reverse order yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
 import random
+from egcd import egcd
 
 
 def power(x, y, p):  # returns (x^y) % p
@@ -62,32 +63,57 @@ def getPrimeOfLength(bits):
 
 
 def getCoPrimeTo(number):
-    e = 65537;
-    while (math.gcd(e, number) != 1):
-        e += 2;
-    return (e);
+    e_candidate = 65537;
+    while (math.gcd(e_candidate, number) != 1):
+        e_candidate += 2;
+    return (e_candidate);
 
 
-length = 4;
-p = getPrimeOfLength(length);
+length = 512;
+p = 7;  # getPrimeOfLength(length);
 print("p = " + str(p));
-q = getPrimeOfLength(length);
-while(p==q):
+q = 11;  # getPrimeOfLength(length);
+while (p == q):
     q = getPrimeOfLength(length);
 print("q = " + str(q));
 
-n = p * q;
+n = p * q;  # public key 1
 phi = (p - 1) * (q - 1);
 
 print("phi  = = (p - 1) * (q - 1) = " + str(phi));
 
-e = getCoPrimeTo(phi);
-print("e = " + str(e));
+e = 17;  # getCoPrimeTo(phi);
+print("e = " + str(e));  # public key 2
+[gcd, d, u] = egcd(e, phi);
+if (d < 0):
+    d = d % phi;
+print("d = " + str(d));  # private key
 
-#numOfPrimes = math.trunc(x / math.trunc(math.log(x)));
+# numOfPrimes = math.trunc(x / math.trunc(math.log(x)));
 
 
+m = 17;
+print("initial m = " + str(m));
+
+def squareAndMultiply(x, k, n):
+    print("k = " + str(bin(k)));
+    res = 1;
+    while (k != 0):
+        k_i = k % 2;
+        k = int((k - k_i) / 2);
+        if (k_i == 0):
+            print("k_i = 0");
+            res = (res * res) % n;
+            res * x  # fake multiplication
+        else:
+            print("k_i = 1");
+            res = (res * res * x) % n;
+    return res;
 
 
+# encipher:
+c = squareAndMultiply(m, e, n);
+print("c = " + str(c));
 
-# qustions: does e need to have the same bit length as p and q?
+deciphered_m = squareAndMultiply(c, d, n);
+print("m = " + str(deciphered_m));
